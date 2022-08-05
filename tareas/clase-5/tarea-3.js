15285705b4e96209445b6f8b62f8102f22183577
Maximiliano Lucero <luceromaximiliano8@gmail.com>
2022-08-04 21:29:35 -0300

    Tarea-3

---
+++ tareas/clase-5/tarea-3.js
@@ -0,0 +1,37 @@
+document.querySelector("#calcular").onclick = function() {
+    const $horas = document.querySelectorAll(".horas-video");
+    const $minutos = document.querySelectorAll(".minutos-video");
+    const $segundos = document.querySelectorAll(".segundos-video");
+    const $resultadoCalculo = document.querySelector("#resultado-calculo");
+
+    let totalHoras = sumarTiempo($horas);
+    let totalMinutos = sumarTiempo($minutos);
+    let totalSegundos = sumarTiempo($segundos);
+
+    if (totalMinutos > 60){
+        totalHoras = parseInt(totalHoras + (totalMinutos / 60));
+        totalMinutos %= 60;
+    }
+
+
+    if (totalSegundos > 60){
+        totalMinutos = parseInt(totalMinutos + (totalSegundos / 60));
+        totalSegundos %= 60;
+    }
+
+    const textoResultado = document.createTextNode(` ${totalHoras} horas ${totalMinutos} minutos y ${totalSegundos} segundos`);
+    const textoStrong = document.createElement('strong');
+    textoStrong.appendChild(textoResultado)
+    $resultadoCalculo.appendChild(textoStrong);
+        
+   
+}
+function sumarTiempo(tiempo){
+    let totalTiempo = 0;
+    for (let i = 0; i < tiempo.length; i++){
+         
+        totalTiempo += Number(tiempo[i].value);
+        
+    }
+    return totalTiempo;
+}
